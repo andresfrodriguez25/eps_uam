dd4f354ddfbde9dd8fd80382f6bb8bf64dc4fe10
Andres <andresf.rodriguez@estudiante.uam.es>
2022-09-20 00:43:08 +0200

    Practica 0.1

--- practica.js
+++ practica.js
@@ -1,4 +1,11 @@
-hi
-Hola
+//hi
+//Hola
 
-Vale creo que acabo de descubrir como actualizar en el repositorio todos los cambios
+//Vale creo que acabo de descubrir como actualizar en el repositorio todos los cambios
+
+function hello(){
+    const hello = 'hola'
+    return hello + ' mundo!!'
+}
+console.log(hello())
+
