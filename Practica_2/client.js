16fddb62ed56af8e06d3d10301451b7a2e5ea183
Andres <andresf.rodriguez@estudiante.uam.es>
2022-10-20 17:52:14 +0200

    Actualizado

---
+++ Practica_2/client.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+const server = 'http://localhost:8080'
+async function testHelloWorld(){
+    const result = await axios.get(server + '/')
+    return result.data // el campo data contendrá el resultado
+}
+const hello = await testHelloWorld()
+console.log('Prueba de conexión, resultado: ' + hello)
--- Practica_2/server.js
+++ Practica_2/server.js
@@ -6,10 +6,14 @@ const app = express()
 
 app.use(bodyParser.json())
      app.use(bodyParser.urlencoded({ extended: false }))
+     app.put()
      app.get('/', (req, res) => {
          res.send('Hello World I am running locally')
          //Importante: Ejecutar res.send siempre para poder
          //completar la petición, y no dejar la conexión pendiente,
          //incluso si no se quiere enviar nada de vuelta
 })
+
 const server = app.listen(PORT, () => console.log("listening at localhost:"+PORT))
+
+
